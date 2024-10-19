8f13676c81182b090eb19eadea005e40707fcd80
meryzennn <85497760+meryzennn@users.noreply.github.com>
2024-10-19 22:48:57 +0700

    add info-booking

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -115,4 +115,55 @@ class Booking extends CI_Controller
             redirect(base_url() . 'booking');
         }
     }
+
+    // booking selesai
+    public function bookingSelesai($where)
+    {
+        // Mengupdate stok dan jumlah dibooking di tabel buku saat proses booking diselesaikan
+        $this->db->query(
+            "UPDATE buku, temp 
+            SET buku.dibooking = buku.dibooking + 1, buku.stok = buku.stok - 1 
+            WHERE buku.id = temp.id_buku"
+        );
+
+        $tglSekarang = date('Y-m-d');
+        $isiBooking = [
+        'id_booking' => $this->ModelBooking->kodeOtomatis('booking', 'id_booking'),
+        'tgl_booking' => date('Y-m-d H:i:s'),
+        'batas_ambil' => date('Y-m-d', strtotime('+2 days', strtotime($tglSekarang))),
+        'id_user' => $where
+        ];
+
+        // Menyimpan ke tabel booking dan detail booking, serta mengosongkan tabel temporari
+        $this->ModelBooking->insertData('booking', $isiBooking);
+        $this->ModelBooking->simpanDetail($where);
+        $this->ModelBooking->kosongkanData('temp');
+
+        // Mengarahkan kembali ke halaman informasi booking
+        redirect(base_url() . 'booking/info');
+    }
+
+    // fungsi info
+    public function info()
+    {
+        $where = $this->session->userdata('id_user');
+        $data['user'] = $this->session->userdata('nama');
+        $data['judul'] = "Selesai Booking";
+        $data['useraktif'] = $this->ModelUser->cekData(['id' => $where])->result();
+        $data['items'] = $this->db->query(
+        "SELECT * 
+        FROM booking bo, booking_detail d, buku bu 
+        WHERE d.id_booking = bo.id_booking 
+        AND d.id_buku = bu.id 
+        AND bo.id_user = '$where'"
+        )->result_array();
+
+    // Memuat tampilan halaman dengan data yang sudah disiapkan
+        $this->load->view('templates/templates-user/header', $data);
+        $this->load->view('booking/info-booking', $data);
+        $this->load->view('templates/templates-user/modal');
+        $this->load->view('templates/templates-user/footer');
+    }
+
+
 }
